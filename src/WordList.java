83187377c167a68e6012cfad0df80487463c7f7e
snehakanaujia <36243213+snehakanaujia@users.noreply.github.com>
2019-03-31 11:42:59 -0400

    Update WordList.java

--- src/WordList.java
+++ src/WordList.java
@@ -6,10 +6,10 @@ import java.io.*;
  * words in the array.
  */
 public class WordList {
-	//Dictionary array that holds words
+	// Dictionary array that holds words
 	private Word[] dictionary = new Word[UsefulConstants.MAXWORDS];
 	
-	//Total words in the dictionary
+	// Total words in the dictionary
 	private int totalWords=0;
 
 	/**
@@ -28,6 +28,8 @@ public class WordList {
 	 */
 	private void readDict (String fileName) {
 		FileInputStream fis;
+		
+		// This try block ensures the file can be found and correctly read in.
 		try {
 			fis = new FileInputStream (fileName);
 			System.err.println ("reading dictionary...");
@@ -44,9 +46,11 @@ public class WordList {
 					buffer[i++] = (char) readChar;
 				}
 				s = new String(buffer,0,i);
+				// Stores word into dictionary array
 				dictionary[totalWords] = new Word(s);
 				totalWords++;
 			}
+			// Outputs how many words were read into the dictionary
 			System.err.println("main dictionary has " + totalWords + " entries.");
 			fis.close();
 		}
@@ -88,7 +92,7 @@ public class WordList {
 			return false;
 		}
 		
-		//Checks that the total word counter is correct (matches length of dictionary array)
+		//Checks that the total word counter is correct as long as it matches the length of the dictionary array
 		int counter = 0;
 		for(int i = 0; i < dictionary.length; i++) {
 			if(dictionary[i] != null) {
